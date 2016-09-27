42ce88e1340d23992bc3b3f17f0ca642a982a490
julesvalles88 <julesvalles88@gmail.com>
2016-09-27 09:31:37 +0200

    Add files via upload

---
+++ table 7.py
@@ -0,0 +1,11 @@
+def table(n):
+    i=1
+    while i<11:
+        a=n*i
+        i=i+1
+        print(a)
+table(9)
+print (" ")
+table(3)
+
+
---
+++ tirage au sort.py
@@ -0,0 +1,8 @@
+from random import*
+tab = [ ]
+i=1
+while i<=10:
+    a = randint(0,100)
+    tab.append(a)
+    i=i+1
+    print(tab)
